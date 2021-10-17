762a70a8e6da3d919746256a6ffa6e78cc63e40d
Xin-Ting Ke <greenpepper0606@gmail.com>
2021-10-17 08:48:40 +0800

    設定 Route 跳轉路由

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 //設定 Route 回傳 view
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 //設定 Route 回傳字串
+/*
 Route::get('/', function() {
     return 'welcome';
+});
+*/
+
+//設定 Route 跳轉路由
+Route::get('r1', function(){
+    return redirect('r2');
+});
+
+Route::get('r2', function(){
+    return view('welcome');
 });
