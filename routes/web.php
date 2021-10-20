3e38d1b17ae4be5256331798c6a316814015ff4d
Xin-Ting Ke <greenpepper0606@gmail.com>
2021-10-20 11:20:52 +0800

    為 Route 命名

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,11 @@ Route::get('/', function () {
 });
 
 //設定 Route 回傳字串
-/*
 Route::get('/', function() {
     return 'welcome';
 });
-*/
 
 //設定 Route 跳轉路由
-/*
 Route::get('r1', function(){
     return redirect('r2');
 });
@@ -34,7 +31,6 @@ Route::get('r1', function(){
 Route::get('r2', function(){
     return view('welcome');
 });
-*/
 
 //修改 Route 接受參數
 /*
@@ -47,7 +43,14 @@ Route::get('hello/{name?}', function($name = 'Everybody'){
 */
 
 //增加新的 Route
+/*
 Route::get('hello/{name}', function($name){
     return 'Hello, '.$name.'. Nice to meet you!';
 });
+*/
+
+//將 Route 取名為 hello.index
+Route::get('hello/{name?}', function($name = 'Everybody'){
+    return 'Hello, '.$name;
+})-> name('hello.index');
 
